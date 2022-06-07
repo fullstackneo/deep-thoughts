6ab62333c9d5fdc245eb219e3bcd4bd9a93106e1
Neo <fullstackneo@gmail.com>
2022-06-07 02:08:58 -0600

    add typeDefs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,17 @@
+const { User, Thought } = require('../models');
+
+const resolvers = {
+  Query: {
+ 
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+  },
+};
+
+module.exports = resolvers;
+// Otha22;
+//    thoughts: async () => {
+//       return Thought.find().sort({ createdAt: -1 });
+//     },
