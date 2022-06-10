dde72f52e48e6dbeb435a4ad5e806a8ade2267fb
Neo <fullstackneo@gmail.com>
2022-06-09 18:08:21 -0600

    add queries to retrieve logged-in user's info

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,6 @@
 import { gql } from '@apollo/client';
 // Apollo Client (@apollo/client) is an all-in-one dependency that enables us to connect to a GraphQL API server and execute queries or mutations using their own special form of React Hooks.
 
-
 // GraphQL (graphql) is a dependency much like MySQL2 was for Sequelize. We don't use it directly, but it needs to be present so that the GraphQL syntax used with Apollo Client can be understood.
 export const QUERY_THOUGHTS = gql`
   query thoughts($username: String) {
@@ -55,6 +54,60 @@ export const QUERY_USER = gql`
         thoughtText
         createdAt
         reactionCount
+      }
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+        reactions {
+          _id
+          createdAt
+          reactionBody
+          username
+        }
+      }
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+// Once you feel more comfortable with GraphQL and Apollo, look up how to use something called "Directives"!
+export const QUERY_ME_BASIC = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+        reactions {
+          _id
+          createdAt
+          reactionBody
+          username
+        }
+      }
+      friends {
+        _id
+        username
       }
     }
   }
