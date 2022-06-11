5401665c523a5c85e6a039e9bba394fc3991ae8c
Neo <fullstackneo@gmail.com>
2022-06-11 01:55:09 -0600

    add reaction component

--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { ADD_REACTION } from '../../utils/mutations';
+import { useMutation } from '@apollo/client';
 
 const ReactionForm = ({ thoughtId }) => {
   const [reactionBody, setBody] = useState('');
   const [characterCount, setCharacterCount] = useState(0);
+  const [addReaction, { error }] = useMutation(ADD_REACTION);
 
   const handleChange = event => {
     if (event.target.value.length <= 280) {
@@ -13,12 +16,26 @@ const ReactionForm = ({ thoughtId }) => {
 
   const handleFormSubmit = async event => {
     event.preventDefault();
-    setBody('');
-    setCharacterCount(0);
+
+    try {
+      await addReaction({ variables: { thoughtId, reactionBody } });
+
+      setBody('');
+      setCharacterCount(0);
+    } catch (e) {
+      console.log(e);
+    }
   };
+
   return (
     <div>
-      <p className="m-0">Character Count: {characterCount}/280</p>
+      <p
+        className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}
+      >
+        Character Count: {characterCount}/280{' '}
+        {error && <span className="ml-2">Something went wrong</span>}
+      </p>
+
       <form
         onSubmit={handleFormSubmit}
         className="flex-row justify-center justify-space-between-md align-stretch"
